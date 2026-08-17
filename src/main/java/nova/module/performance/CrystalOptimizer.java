313f6c0cbc03222e9e7c4125b261acff2c9c54eb
IShowrnf <carlsonf188@gmail.com>
2026-08-16 20:11:08 -0500

    Add Crystal Optimizer module scaffold

---
+++ src/main/java/nova/module/performance/CrystalOptimizer.java
@@ -0,0 +1,10 @@
+package nova.module.performance;
+
+import nova.module.Module;
+
+/** Client-side crystal rendering/entity-processing optimization hooks. */
+public final class CrystalOptimizer extends Module {
+    public CrystalOptimizer() {
+        super("Crystal Optimizer", Category.PERFORMANCE);
+    }
+}
